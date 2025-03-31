319b7b44a165785528689ce6f50d8db35df1fa57
Arpita Das <arpita1477@gmail.com>
2025-03-31 17:46:36 +0600

    4A

---
+++ 4A_watermelon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int w;
+    scanf("%d",&w);
+    if(w%2==1)
+      printf("NO\n");
+     else
+     {
+        if(w==2)
+        printf("NO\n");
+        else
+        printf("YES\n");
+     } 
+}
